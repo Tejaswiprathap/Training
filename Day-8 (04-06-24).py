35f2071ff2822fa04580abfc9b390bce0984180e
Tejaswiprathap <120012247+Tejaswiprathap@users.noreply.github.com>
2024-06-06 09:14:34 +0530

    Add files via upload

---
+++ Day-8 (04-06-24).py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Trees
+
+# In[ ]:
+
+
+#input: 2
+#polikujmnhytgbvfredcxswqaz
+#abcd
+
+#qwryupcsfohghjkldezxvbintma
+#ativedoc
+
+#Output:
+#bdca
+#codevita
+
+
+# In[1]:
+
+
+tCase=int(input())
+for i in range(tCase):
+  lexi=input()
+  s=input()
+  for i in range(len(lexi)):
+    c=lexi[i]
+    if c in s:
+      print(c,end='')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
